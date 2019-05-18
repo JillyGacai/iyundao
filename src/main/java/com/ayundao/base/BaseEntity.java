3c5915ac64e9bfc796af7f38a9bb54c81455913b
DLious <85938219@qq.com>
2019-05-18 13:40:36 +0800

    fix: BaseEntity.toString修改
    Signed-off-by: DLious <85938219@qq.com>

--- src/main/java/com/ayundao/base/BaseEntity.java
+++ src/main/java/com/ayundao/base/BaseEntity.java
@@ -3,6 +3,7 @@ package com.ayundao.base;
 import com.ayundao.base.utils.ClassUtils;
 import com.ayundao.base.utils.TimeUtils;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -30,213 +31,220 @@ public abstract class BaseEntity<ID extends Serializable> implements Serializabl
     private static final long serialVersionUID = -67188388306700736L;
 
     /**
-	 * "ID"属性名称
-	 */
-	public static final String ID_PROPERTY_NAME = "id";
+     * "ID"属性名称
+     */
+    public static final String ID_PROPERTY_NAME = "id";
     /**
-	 * "创建日期"属性名称
-	 */
-	public static final String CREATED_DATE_PROPERTY_NAME = "createdDate";
+     * "创建日期"属性名称
+     */
+    public static final String CREATED_DATE_PROPERTY_NAME = "createdDate";
     /**
-	 * "最后修改日期"属性名称
-	 */
-	public static final String LAST_MODIFIED_DATE_PROPERTY_NAME = "lastModifiedDate";
+     * "最后修改日期"属性名称
+     */
+    public static final String LAST_MODIFIED_DATE_PROPERTY_NAME = "lastModifiedDate";
     /**
-	 * "版本"属性名称
-	 */
-	public static final String VERSION_PROPERTY_NAME = "version";
-	@Transient
+     * "版本"属性名称
+     */
+    public static final String VERSION_PROPERTY_NAME = "version";
+    @Transient
     protected final transient Log logger = LogFactory.getLog(this.getClass());
-	/**
-	 * ID
-	 */
-	@JsonView(BaseView.class)
+    /**
+     * ID
+     */
+    @JsonView(BaseView.class)
     @Id
     @GeneratedValue(generator = "jpa-uuid")
     @GenericGenerator(name = "jpa-uuid",
             strategy = "uuid")
     @Column(name = "ID", length = 32)
-	private ID id;
-	/**
-	 * 创建日期
-	 */
-	@Column(name = "CREATEDATE", nullable = false, updatable = false)
-	private String createdDate;
-	/**
-	 * 最后修改日期
-	 */
-	@Column(name = "LASTMODIFIEDTIME", nullable = false)
-	private String lastModifiedDate;
-	/**
-	 * 版本
-	 */
-	@Version
-	@Column(name = "VERSION", nullable = false)
-	private Long version;
+    private ID id;
+    /**
+     * 创建日期
+     */
+    @Column(name = "CREATEDATE", nullable = false, updatable = false)
+    private String createdDate;
+    /**
+     * 最后修改日期
+     */
+    @Column(name = "LASTMODIFIEDTIME", nullable = false)
+    private String lastModifiedDate;
+    /**
+     * 版本
+     */
+    @Version
+    @Column(name = "VERSION", nullable = false)
+    private Long version;
+    public BaseEntity() {
+    }
 
-	/**
-	 * 获取ID
-	 *
-	 * @return ID
-	 */
-	public ID getId() {
-		return id;
-	}
+    /**
+     * 获取ID
+     *
+     * @return ID
+     */
+    public ID getId() {
+        return id;
+    }
 
-	/**
-	 * 设置ID
-	 *
-	 * @param id
-	 *            ID
-	 */
-	private void setId(ID id) {
-		this.id = id;
-	}
+    /**
+     * 设置ID
+     *
+     * @param id ID
+     */
+    private void setId(ID id) {
+        this.id = id;
+    }
 
-	/**
-	 * 获取创建日期
-	 *
-	 * @return 创建日期
-	 */
-	public String getCreatedDate() {
-		return createdDate;
-	}
+    /**
+     * 获取创建日期
+     *
+     * @return 创建日期
+     */
+    public String getCreatedDate() {
+        return createdDate;
+    }
 
-	/**
-	 * 设置创建日期
-	 *
-	 * @param createdDate
-	 *            创建日期
-	 */
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = TimeUtils.setTime(createdDate);
-	}
+    /**
+     * 设置创建日期
+     *
+     * @param createdDate 创建日期
+     */
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = TimeUtils.setTime(createdDate);
+    }
 
-	/**
-	 * 获取最后修改日期
-	 *
-	 * @return 最后修改日期
-	 */
-	public String getLastModifiedDate() {
-		return lastModifiedDate;
-	}
+    /**
+     * 获取最后修改日期
+     *
+     * @return 最后修改日期
+     */
+    public String getLastModifiedDate() {
+        return lastModifiedDate;
+    }
 
-	/**
-	 * 设置最后修改日期
-	 *
-	 * @param lastModifiedDate
-	 *            最后修改日期
-	 */
-	public void setLastModifiedDate(Date lastModifiedDate) {
-		this.lastModifiedDate = TimeUtils.setTime(lastModifiedDate);
-	}
+    /**
+     * 设置最后修改日期
+     *
+     * @param lastModifiedDate 最后修改日期
+     */
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = TimeUtils.setTime(lastModifiedDate);
+    }
 
-	/**
-	 * 获取版本
-	 *
-	 * @return 版本
-	 */
-	public Long getVersion() {
-		return version;
-	}
+    /**
+     * 获取版本
+     *
+     * @return 版本
+     */
+    public Long getVersion() {
+        return version;
+    }
 
-	/**
-	 * 设置版本
-	 *
-	 * @param version
-	 *            版本
-	 */
-	public void setVersion(Long version) {
-		this.version = version;
-	}
+    /**
+     * 设置版本
+     *
+     * @param version 版本
+     */
+    public void setVersion(Long version) {
+        this.version = version;
+    }
 
-	/**
-	 * 判断是否为新建对象
-	 *
-	 * @return 是否为新建对象
-	 */
-	@Transient
-	public boolean isNew() {
-		return getId() == null;
-	}
+    /**
+     * 判断是否为新建对象
+     *
+     * @return 是否为新建对象
+     */
+    @Transient
+    public boolean isNew() {
+        return getId() == null;
+    }
 
-	/**
-	 * 重写toString方法
-	 *
-	 * @return 字符串
-	 */
-	@Override
-	public String toString() {
+    /**
+     * 重写toString方法
+     *
+     * @return 字符串
+     */
+    @Override
+    public String toString() {
         ToStringBuilder builder = new ToStringBuilder(this);
         Iterator var3 = ClassUtils.getDeclaredFieldsWithSuper(this.getClass()).values().iterator();
 
-        while(var3.hasNext()) {
-            Field field = (Field)var3.next();
+        while (var3.hasNext()) {
+            Field field = (Field) var3.next();
+            //基类
             Class<? extends Object> typeClazz = field.getType();
-            if (!AbstractEntity.class.isAssignableFrom(typeClazz) && !Collection.class.isAssignableFrom(typeClazz) && !Map.class.isAssignableFrom(typeClazz)) {
-                int modifiers = field.getModifiers();
-                if (field.getName().indexOf(36) == -1 && !Modifier.isStatic(modifiers) && !Modifier.isTransient(modifiers)) {
+//            if (!AbstractEntity.class.isAssignableFrom(typeClazz) && !Collection.class.isAssignableFrom(typeClazz) && !Map.class.isAssignableFrom(typeClazz)) {
+//                int modifiers = field.getModifiers();
+//                if (field.getName().indexOf(36) == -1 && !Modifier.isStatic(modifiers) && !Modifier.isTransient(modifiers)) {
                     builder.append(field.getName(), ClassUtils.forceGetProperty(this, field.getName()));
+//                }
+//            }
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 重写equals方法
+     *
+     * @param other 对象
+     * @return 是否相等
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        } else if (this.getClass().isInstance(other) && other.getClass().isInstance(this)) {
+            EqualsBuilder builder = new EqualsBuilder();
+            Iterator var4 = ClassUtils.getDeclaredFieldsWithSuper(this.getClass()).values().iterator();
+
+            while (var4.hasNext()) {
+                Field field = (Field) var4.next();
+                Class<? extends Object> typeClazz = field.getType();
+                if (!AbstractEntity.class.isAssignableFrom(typeClazz) && !Collection.class.isAssignableFrom(typeClazz) && !Map.class.isAssignableFrom(typeClazz)) {
+                    int modifiers = field.getModifiers();
+                    if (field.getName().indexOf(36) == -1 && !Modifier.isStatic(modifiers) && !Modifier.isTransient(modifiers)) {
+                        builder.append(ClassUtils.forceGetProperty(this, field.getName()), ClassUtils.forceGetProperty(other, field.getName()));
+                    }
                 }
             }
+
+            return builder.isEquals();
+        } else {
+            return false;
         }
+    }
 
-        return builder.toString();
-	}
+    /**
+     * 重写hashCode方法
+     *
+     * @return HashCode
+     */
+    @Override
+    public int hashCode() {
+        int hashCode = 17;
+        hashCode += getId() != null ? getId().hashCode() * 31 : 0;
+        return hashCode;
+    }
 
-	/**
-	 * 重写equals方法
-	 *
-	 * @param obj
-	 *            对象
-	 * @return 是否相等
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (this == obj) {
-			return true;
-		}
-		if (!BaseEntity.class.isAssignableFrom(obj.getClass())) {
-			return false;
-		}
-		BaseEntity<?> other = (BaseEntity<?>) obj;
-		return getId() != null ? getId().equals(other.getId()) : false;
-	}
+    /**
+     * 保存验证组
+     */
+    public interface Save extends Default {
 
-	/**
-	 * 重写hashCode方法
-	 *
-	 * @return HashCode
-	 */
-	@Override
-	public int hashCode() {
-		int hashCode = 17;
-		hashCode += getId() != null ? getId().hashCode() * 31 : 0;
-		return hashCode;
-	}
+    }
 
-	/**
-	 * 保存验证组
-	 */
-	public interface Save extends Default {
+    /**
+     * 更新验证组
+     */
+    public interface Update extends Default {
 
-	}
+    }
 
-	/**
-	 * 更新验证组
-	 */
-	public interface Update extends Default {
+    /**
+     * 基础视图
+     */
+    public interface BaseView {
 
-	}
-
-	/**
-	 * 基础视图
-	 */
-	public interface BaseView {
-
-	}
+    }
 
 }
--- src/main/java/com/ayundao/base/utils/ClassUtils.java
+++ src/main/java/com/ayundao/base/utils/ClassUtils.java
@@ -4,8 +4,11 @@ import com.ayundao.base.BaseEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.Transient;
 import java.lang.reflect.Field;
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 /**
  * @ClassName: ClassUtils
@@ -17,26 +20,123 @@ import java.util.Map;
  */
 public class ClassUtils {
 
+    @Transient
     private static final Logger logger = LoggerFactory.getLogger(ClassUtils.class);
 
+    /**
+     * 强制获取字段的value
+     * @param obj
+     * @param name
+     * @return
+     */
     public static String forceGetProperty(Object obj, String name) {
+        Class<?> cls = obj.getClass();
+        String[] fieldNames = new String[]{"id", "version", "createdDate", "lastModifiedDate"};
         String result = "";
+        String mn = "";
+        Method method = null;
         try {
-            Field[] fields = obj.getClass().getFields();
+            Field[] fields = obj.getClass().getDeclaredFields();
+            for (String s : fieldNames) {
+                if (name.equals(s)) {
+                    fields = obj.getClass().getSuperclass().getDeclaredFields();
+                    break;
+                } 
+            }
             for (Field field : fields) {
                 if (field.getName().equals(name)) {
                     field.setAccessible(true);
-                    result = field.getName();
+                    mn = "get" + toFirstUpperCase(field.getName());
+                    method = cls.getMethod(mn, new Class[]{});
+                    obj = method.invoke(obj, new Object[]{});
+                    result = obj.toString();
                 }
             }
         } catch (NullPointerException e) {
-            logger.error(e.getLocalizedMessage());
+            logger.error(e.getMessage());
+        } catch (NoSuchMethodException e) {
+            logger.error(e.getMessage());
+        } catch (IllegalAccessException e) {
+            logger.error(e.getMessage());
+        } catch (InvocationTargetException e) {
+            logger.error(e.getMessage());
         }
         return result;
     }
 
-    public static Map<String, Object> getDeclaredFieldsWithSuper(Class<? extends BaseEntity> clazz) {
-        return null;
+    /**
+     * 获取基类和子类的所有字段属性
+     * @param clazz
+     * @return
+     */
+    public static Map<String, Field> getDeclaredFieldsWithSuper(Class<? extends BaseEntity> clazz) {
+        Class superClass = clazz.getSuperclass();
+        List<Method> methods = getAllMethods(clazz);
+        Map<String, Field> map = new LinkedHashMap<>();
+        Field[] fields = null;
+        Field field = null;
+        //基类
+        map = getAllFields(map, methods, clazz.getSuperclass());
+        map = getAllFields(map, methods, clazz);
+        return map;
+    }
+
+    /**
+     * 获取基类与子类的所有字段(set/get)
+     * @param map
+     * @param methods
+     * @param cls
+     * @return
+     */
+    public static Map<String, Field> getAllFields(Map<String, Field> map, List<Method> methods, Class<?> cls) {
+        Field[] fields = cls.getDeclaredFields();
+        for (Method m : methods) {
+            for (Field f : fields) {
+                if (f.getName().equals(toFirstLowwerCase(m.getName().substring(3, m.getName().length())))) {
+                    map.put(f.getName(), f);
+                }
+            }
+        }
+        return map;
+    }
+
+    /**
+     * 获取基类和子类的所有方法(set/get)
+     * @param cls
+     * @return
+     */
+    public static List<Method> getAllMethods(Class cls) {
+        List<Method> methods = new LinkedList<>();
+        for (Method method : cls.getSuperclass().getDeclaredMethods()) {
+            if (method.getName().startsWith("set")) {
+                methods.add(method);
+            }
+        }
+        for (Method method : cls.getDeclaredMethods()) {
+            if (method.getName().startsWith("set")) {
+                methods.add(method);
+            }
+        }
+        return methods;
+    }
+
+
+    /**
+     * 首字母大写
+     * @param name
+     * @return
+     */
+    public static String toFirstUpperCase(String name) {
+        return name.substring(0, 1).toUpperCase() + name.substring(1, name.length());
+    }
+
+    /**
+     * 首字母小写
+     * @param name
+     * @return
+     */
+    public static String toFirstLowwerCase(String name) {
+        return name.substring(0, 1).toLowerCase() + name.substring(1, name.length());
     }
 
 }
